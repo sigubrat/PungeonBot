9a2aad7df25c26ea073d5cb9bae8cb6336092e52
sigubrat <sigurd.brattland@fangstr.no>
2023-08-20 16:08:12 +0200

    Change parameters and output

--- PungeounBot.py
+++ PungeounBot.py
@@ -40,23 +40,39 @@ async def on_ready():
 
 @client.tree.command()
 @app_commands.describe(
-    rolls="How many times do you want to roll?",
-    sides="Which die are you rolling? (d4, d6, d8, d10, d12, d20)"
+    dice="What dice you want to use (1d20, 1d6, 2d4 etc)",
+    modifier="Optional: roll modifier"
 )
-async def roll(interaction: discord.Interaction, rolls: int, sides: int):
-    """Rolls a dice in NdN format (d4, d6, d8, d10, d12, d20)"""
+async def roll(interaction: discord.Interaction, dice: str, modifier: int = 0):
+    """Rolls a dice in NdN format where N is a number"""
+    dice.replace("D", "d")
+    try:
+        rolls, d = map(int, dice.split('d'))
+    except Exception:
+        await interaction.response.send_message("Format must be in NdN e.g., 1d20")
+        return
     
     if rolls < 1:
         await interaction.response.send_message(f"Minimum 1 roll is required you silly sausage :)")
 
-    if sides not in [4, 6, 8, 10, 12, 20]:
-        await interaction.response.send_message(f"The die you provided ({rolls}d{sides}) does not match any DnD standard die")
+    if d not in [4, 6, 8, 10, 12, 20]:
+        await interaction.response.send_message(f"The die you provided ({rolls}d{d}) does not match any DnD standard die")
 
-    roll_results = [random.randint(1, sides) for _ in range(rolls)]
+    roll_results = [random.randint(1, d) for _ in range(rolls)]
     total = sum(roll_results)
+    total += modifier
     rolls_string = ", ".join(map(str, roll_results))
-    result = f"[{rolls_string}] = {total}"
-    await interaction.response.send_message(result)
+    result = f"[{rolls_string}]"
+    
+    if modifier > 0:
+        result += f" +{modifier}"
+    elif modifier < 0:
+        result += f" {modifier}"
+    
+    result += f" = {total}"
+
+    await interaction.response.send_message(f"```{interaction.user.display_name} rolled: {result}```")
+    print(f"{interaction.user} rolled {result}")
 
 
 client.run(token)
